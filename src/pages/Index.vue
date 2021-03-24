001f42e30222e62ee6003cd159bd0d99eb013ba8
SyogoShibuya <hotococoa0204@icloud.com>
2021-03-24 09:32:13 +0900

    title修正

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,31 +1,29 @@
 <template>
   <Layout>
-
     <About />
     <Skills />
     <Education />
     <Contact />
-
   </Layout>
 </template>
 
 <script>
-import About from '@/components/About.vue'
-import Skills from '@/components/Skills.vue'
-import Education from '@/components/Education.vue'
-import Contact from '@/components/Contact.vue'
+import About from "@/components/About.vue";
+import Skills from "@/components/Skills.vue";
+import Education from "@/components/Education.vue";
+import Contact from "@/components/Contact.vue";
 
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: "SyogoShibuya.portfolio",
   },
   components: {
     About,
     Skills,
     Education,
     Contact,
-  }
-}
+  },
+};
 </script>
 
 <style>
